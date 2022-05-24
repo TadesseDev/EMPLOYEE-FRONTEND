d6c63bad1777a7e245af2a1caff9cb6dce4c49ff
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-24 11:38:29 +0300

    Deploye 24-66-2022

--- src/Redux-Saga/worker/workers.ts
+++ src/Redux-Saga/worker/workers.ts
@@ -1,23 +1,27 @@
 import { getEmployees,deleteEmployee,createEmployee,updateCurrentEmployee } from '../../API/employee.tsx'
-import { put } from 'redux-saga/effects'
-function* updateEmployees(action: Object) {
+import { put } from 'redux-saga/effects';
+import { EmployeeType} from '../../Global/types.ts'
+
+function* updateEmployees(action: EmployeeType) {
   const data = yield (getEmployees())
   yield put({ type: `${action.type}_READY`, payload: data });
 }
 
-function* removeEmployee(action: Object) {
+function* removeEmployee(action: EmployeeType) {
   const data = yield (deleteEmployee(action.payload.empId))
   yield put({ type: `${action.type}_READY`, payload: action.payload });
+  return data;
 }
-function* addNewUSer(action: Object) {
+function* addNewUSer(action: EmployeeType) {
   const data = yield (createEmployee(action.payload));
   yield put({ type: `${action.type}_READY`, payload: data });
+  return data;
 }
 
-function* updateUserData(action: Object) {
-  console.log('updating user');
+function* updateUserData(action: EmployeeType) {
   const data = yield (updateCurrentEmployee(action.payload))
   yield put({ type: `${action.type}_READY`, payload: action.payload });
+  return data;
 }
 
 export { updateEmployees, removeEmployee,addNewUSer,updateUserData }
